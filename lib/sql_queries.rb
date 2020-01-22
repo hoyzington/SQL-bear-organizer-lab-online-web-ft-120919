4ebe3e9cc206b537560a4216825704d675e13cd9
Scott Hoisington <hoyzington@gmail.com>
2020-01-22 06:45:00 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,13 +19,13 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  ""
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  ""
 end
 
 def selects_bear_that_killed_Tim
-  "SELECT "
+  "SELECT * FROM bears WHERE name = NULL"
 end
